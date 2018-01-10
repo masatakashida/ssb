2a9baf7488d496394c4e212060fc5802287dfe83
masataka <ar.tec.masa@gmail.com>
2018-01-10 14:03:15 +0900

    ユーザー関係

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110045955) do
+ActiveRecord::Schema.define(version: 20180110050207) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20180110045955) do
     t.integer "follwed_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["follwer_id", "follwed_id"], name: "index_relationships_on_follwer_id_and_follwed_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
