a0d9f0d68abb4aca9a86fe3be19a1c5cf332f064
masataka <ar.tec.masa@gmail.com>
2018-01-10 13:22:01 +0900

    カテゴリモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110041609) do
+ActiveRecord::Schema.define(version: 20180110042058) do
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.string "image", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "book_name", default: "", null: false
